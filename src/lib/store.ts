b86ddd7e5fea959f9e8669f53fba9f00045c60c4
ehsan <m.ehsan1381@gmail.com>
2024-10-18 12:52:30 +0330

    config redux

---
+++ src/lib/store.ts
@@ -0,0 +1,16 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import loginApi from './features/auth/login-api-slice';
+
+const rootReducer = combineReducers({ loginApi: loginApi.reducer });
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(loginApi.middleware),
+  });
+};
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
